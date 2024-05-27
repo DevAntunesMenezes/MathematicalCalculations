dc82d0fd7fbafa4778e1692777897677bea3a7c2
Antunes Menezes <167531998+DevAntunesMenezes@users.noreply.github.com>
2024-05-27 20:54:28 -0300

    Update script.js

--- Equação do 2º grau/js/script.js
+++ Equação do 2º grau/js/script.js
@@ -2,6 +2,10 @@ function calcularRaizes() {
   const a = parseFloat(document.getElementById("coefA").value);
   const b = parseFloat(document.getElementById("coefB").value);
   const c = parseFloat(document.getElementById("coefC").value);
+  const delta = b * b - 4 * a * c;
+  const raizUnica = -b / (2 * a);
+  const raiz1 = (-b + Math.sqrt(delta)) / (2 * a);
+  const raiz2 = (-b - Math.sqrt(delta)) / (2 * a);
 
   // Verificação de NaN
   if (isNaN(a) || isNaN(b) || isNaN(c)) {
@@ -9,19 +13,13 @@ function calcularRaizes() {
       "Por favor, insira valores válidos para os coeficientes.";
     return;
   }
-
-  const delta = b * b - 4 * a * c;
-
   if (delta < 0) {
     document.getElementById("result").innerText = `Não há raizes reais.`;
   } else if (delta === 0) {
-    const raizUnica = -b / (2 * a);
     document.getElementById(
       "result"
     ).innerText = `A única raíz é ${raizUnica}.`;
   } else {
-    const raiz1 = (-b + Math.sqrt(delta)) / (2 * a);
-    const raiz2 = (-b - Math.sqrt(delta)) / (2 * a);
     document.getElementById(
       "result"
     ).innerText = `As raízes são: ${raiz1} e ${raiz2}.`;
